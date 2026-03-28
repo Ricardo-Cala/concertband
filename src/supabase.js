bfef5b84f7ff911f4c17d78eed76b44817389411
Ricardo-Cala <rcalasanz@caatvalencia.es>
2026-03-28 13:26:32 +0100

    variables de entorno para produccion

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://lnnbivrwtvsncsikjbbz.supabase.co'
-const supabaseKey = 'sb_publishable_yecsIAzUInTzf_PNv3fn4g_lUbKKmwH'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
